4e612d4c68120fe2c0ed002e5069b28838ef9f20
Georg Reich <39133020+1b15@users.noreply.github.com>
2020-10-08 11:55:33 +0200

    Implemented TODO: Copy any resources

--- echo_kernel/install.py
+++ echo_kernel/install.py
@@ -2,6 +2,7 @@ import argparse
 import json
 import os
 import sys
+import shutil
 
 from jupyter_client.kernelspec import KernelSpecManager
 from IPython.utils.tempdir import TemporaryDirectory
@@ -17,9 +18,16 @@ def install_my_kernel_spec(user=True, prefix=None):
         os.chmod(td, 0o755) # Starts off as 700, not user readable
         with open(os.path.join(td, 'kernel.json'), 'w') as f:
             json.dump(kernel_json, f, sort_keys=True)
-        # TODO: Copy any resources
-
         print('Installing Jupyter kernel spec')
+        
+        # requires logo files in kernel root directory
+        cur_path = os.path.dirname(os.path.realpath(__file__))
+        for logo in ["logo-32x32.png", "logo-64x64.png"]:
+            try:
+                shutil.copy(os.path.join(cur_path, logo), td)
+            except FileNotFoundError:
+                print("Custom logo files not found. Default logos will be used.")
+        
         KernelSpecManager().install_kernel_spec(td, 'echo', user=user, replace=True, prefix=prefix)
 
 def _is_root():
